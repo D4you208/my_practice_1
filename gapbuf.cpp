4b776ed45b20ff9a2ca2a2dd29d69ccadb7b7a20
D4you208 <39988195+D4you208@users.noreply.github.com>
2018-11-02 01:32:40 +0900

    Update gapbuf.cpp

--- gapbuf.cpp
+++ gapbuf.cpp
@@ -9,6 +9,8 @@ struct gapbuf
 	{
 		g_start = 0;
 		g_end = max_glen - 1;
+		for( int i = 0 ; i < max_glen ; i++)
+			buff[i] = '\0';
 	}
 };
 bool gapbuf_full( gapbuf *gbuf )
@@ -39,11 +41,27 @@ void insert ( gapbuf *gbuf , char ch )
 {
 	if( gapbuf_full( gbuf ) )
 		return;
-	gbuf->gbuf[gbuf->g_start++] = ch;
+	gbuf->buff[gbuf->g_start++] = ch;
 }
 char delete_char( gapbuf *gbuf )
 {
 	if( gapbuf_empty ( gbuf ))
 		return '\0';
-	gbuf->buff[--g_start] = '\0';
+	gbuf->buff[--gbuf->g_start] = '\0';
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	gapbuf* gbuf = new gapbuf();
+	insert (  gbuf , 'D');
+	insert (  gbuf , 'E');
+	insert (  gbuf , 'E');
+	insert (  gbuf , 'P');
+	insert (  gbuf , 'A');
+	insert (  gbuf , 'K');
+	
+	cout << gbuf->buff;
+	while ( 1 );
 }
